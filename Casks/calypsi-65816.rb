77c0d4efa667e1d93059b5f8a504da48a6d7d0b3
Jesus Garcia <daschewie@gmail.com>
2023-06-28 08:26:16 -0400

    Update calypsi-65816.rb

--- Casks/calypsi-65816.rb
+++ Casks/calypsi-65816.rb
@@ -1,6 +1,6 @@
 cask "calypsi-65816" do
-  version "3.6.12"
-  sha256 "bc732e4fe98ebac30495f54261e3ef59ca44a4a320b03513e81452c6282b35db"
+  version "4.4"
+  sha256 "937a1a94379341792fb5ef8cf8abcb654fe34b2867698bfda88d6fcb4865457f"
 
   url "https://github.com/hth313/Calypsi-tool-chains/releases/download/#{version}/calypsi-65816-#{version}.pkg"
   name "calypsi-65816"
