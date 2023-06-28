21f60df23fc096d59877f07cfdfef073a59d5ff7
Jesus Garcia <daschewie@gmail.com>
2023-06-28 08:28:21 -0400

    Update calypsi-68k.rb

--- Casks/calypsi-68k.rb
+++ Casks/calypsi-68k.rb
@@ -1,8 +1,8 @@
 cask "calypsi-68k" do
-  version "3.6.12"
-  sha256 "2f3975a4faf7a14dd97d4f3bae4ff2b21aedc58be8bf5cbe713d76ba7946a29d"
+  version "4.4"
+  sha256 "043bae3a009744f5a64ee506f506a7193562354dc68b9becd9f0c3421f75d966"
 
-  url "https://github.com/hth313/Calypsi-tool-chains/releases/download/#{version}/calypsi-68k-#{version}.pkg"
+  url "https://github.com/hth313/Calypsi-tool-chains/releases/download/#{version}/calypsi-68000-#{version}.pkg"
   name "calypsi-68k"
   desc "Calypsi is a C and assembly language cross compiler tool chains for M68000."
   homepage "https://www.calypsi.cc/"
